017ac506c3d4e9c268df8bf2028c98e992f50e5f
JuliaPriadka <150530521+JuliaPriadka@users.noreply.github.com>
2024-03-12 21:47:01 +0200

    Changed task-3 & task-5

--- js/task-3.js
+++ js/task-3.js
@@ -3,7 +3,7 @@ const headerTextEl = document.querySelector("#name-output");
 
 inputEl.addEventListener("input", (event) => {
   const inputValue = event.currentTarget.value.trim();
-  if (event.currentTarget.value === " ") {
+  if (event.currentTarget.value === "" || event.currentTarget.value.includes(" ")) {
     headerTextEl.textContent = "Anonymous";
     return;
   }
--- js/task-5.js
+++ js/task-5.js
@@ -1,15 +1,17 @@
-const buttonEl = document.querySelector(".change-color");
-buttonEl.addEventListener("click", changeBgdColor);
+const button = document.querySelector(".change-color");
 
-const textAccent = document.querySelector(".color");
+const colorSpan = document.querySelector(".color");
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
-
+  }
+  
 function changeBgdColor(event) {
-  document.body.style.backgroundColor = getRandomHexColor();
-  textAccent.textContent = document.body.style.backgroundColor;
+    const randomColor = getRandomHexColor()
+    document.body.style.backgroundColor = randomColor;
+    colorSpan.textContent = randomColor;
 }
+  
+  button.addEventListener("click", changeBgdColor);
