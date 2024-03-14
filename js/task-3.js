216b31f9cfb4cf8a3ff7ba1665ea25bdd1e5caea
JuliaPriadka <150530521+JuliaPriadka@users.noreply.github.com>
2024-03-14 22:24:32 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,6 @@
 const inputEl = document.querySelector("#name-input");
 const headerTextEl = document.querySelector("#name-output");
 
-inputEl.addEventListener("input", (event) => {
-  const inputValue = event.currentTarget.value.trim();
-  if (inputValue === "") {
-    inputValue = "Anonymous";
-  }
-  headerTextEl.textContent = inputValue;
+inputEl.addEventListener("input", () => {
+  headerTextEl.textContent = inputEl.ariaValueMax.trim() || "Anonymous";
 });
